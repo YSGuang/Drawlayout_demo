88cc781786ea4b33df8be398d1eccb0cf9651613
YuShuaiGuang <1019418872@qq.com>
2017-07-24 08:10:44 +0800

    Initial commit

---
+++ app/src/main/java/yu/baway/com/drawlayout_demo/MenuLeftFragment.java
@@ -0,0 +1,19 @@
+package yu.baway.com.drawlayout_demo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by YU on 2017/7/23.
+ */
+public class MenuLeftFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.layout_menu, container, false);
+    }
+}
